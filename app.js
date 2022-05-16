a6afd2c6532aa77c9a3d2d72ac5fd4fe4526f68c
igips <igor.szepietowski@gmail.com>
2022-05-16 16:33:22 +0200

    Add databse

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 
 const app = express();
+const mongoose = require("mongoose");
+
+const mongoDB =
+	"mongodb+srv://user1:user1@fridge-inventory.w34eg.mongodb.net/inventory_app?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
