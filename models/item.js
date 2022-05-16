fa9a39164bca71d31d2e9af5e95d0abf25a84471
igips <igor.szepietowski@gmail.com>
2022-05-16 16:34:06 +0200

    Create model

---
+++ models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema ({
+    name: {type: String, required: true, maxlength: 30},
+    brand: {type: String, required: true, maxlength: 30},
+    category: {type: Schema.Types.ObjectId, ref: "Category"},
+    stock: {type: Number, required: true, maxlength: 3}
+});
+
+ItemSchema.virtual("url").get(function () {
+    return "/items/" + this._id;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
