4c03a25412b547f53a4d6eccd17a8ec5acf5d8ad
igips <igor.szepietowski@gmail.com>
2022-05-17 19:16:54 +0200

    Refactor

--- models/category.js
+++ models/category.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Item = require("../models/item");
 
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-    name: {type: String, required: true, maxlength: 30}
+	name: { type: String, required: true, maxlength: 30 },
 });
 
-
 CategorySchema.virtual("url").get(function () {
-    return "/categories/" + this._id;
+	return "/categories/" + this._id;
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
