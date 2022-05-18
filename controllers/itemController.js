c2ff9a2fca896699b60a102c67654b377fe8ee6b
igips <igor.szepietowski@gmail.com>
2022-05-18 17:59:25 +0200

    Create item_add_get and post functions

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,7 @@
 const Item = require("../models/item");
 const Category = require("../models/category");
 const async = require("async");
+const { body, validationResult } = require("express-validator");
 
 exports.index = (req, res) => {
 	async.parallel(
@@ -46,13 +47,98 @@ exports.item_details = (req, res, next) => {
 		});
 };
 
-exports.item_add_get = (req, res) => {
-	res.send("NOT IMPLEMENTED: Item add GET");
+exports.item_add_get = (req, res, next) => {
+	Category.find({}).exec((err, results) => {
+		if (err) {
+			return next(err);
+		}
+
+		results.sort((a, b) => a.name.localeCompare(b.name));
+
+		res.render("itemForm", { title: "Add item", categories: results, item: "", errors: "" });
+	});
 };
 
-exports.item_add_post = (req, res) => {
-	res.send("NOT IMPLEMENTED: Item add POST");
-};
+exports.item_add_post = [
+	body("name").trim().escape(),
+	body("brand").trim().escape(),
+	body("category").escape(),
+	body("stock").trim().escape(),
+	(req, res, next) => {
+		const errors = validationResult(req);
+
+		let item;
+
+		if (req.body.category !== "") {
+			item = new Item({
+				name: req.body.name,
+				brand: req.body.brand,
+				category: req.body.category,
+				stock: req.body.stock,
+			});
+		} else {
+			item = new Item({
+				name: req.body.name,
+				brand: req.body.brand,
+				stock: req.body.stock,
+			});
+		}
+
+		if (!errors.isEmpty()) {
+			Category.find({}).exec((err, results) => {
+				if (err) {
+					return next(err);
+				}
+				results.sort((a, b) => a.name.localeCompare(b.name));
+
+				res.render("itemForm", { title: "Add item", categories: results, item: item, errors: errors.array() });
+			});
+			return;
+		} else {
+			const theName = req.body.name;
+			const theBrand = req.body.brand;
+
+			Item.findOne({ name: { $regex: theName, $options: "i" }, brand: { $regex: theBrand, $options: "i" } }).exec(
+				(err, found_item) => {
+					if (err) {
+						return next(err);
+					}
+
+					if (found_item) {
+						if (
+							(found_item.category && req.body.category !== found_item.category) ||
+							found_item.stock !== req.body.stock
+						) {
+							Item.findByIdAndUpdate(
+								found_item._id,
+								{
+									stock: req.body.stock,
+									category: req.body.category !== "" ? req.body.category : found_item.category,
+								},
+								(err, theItem) => {
+									if (err) {
+										return next(err);
+									}
+									res.redirect(theItem.url);
+								}
+							);
+						} else {
+							res.redirect(found_item.url);
+						}
+					} else {
+						item.save((err) => {
+							if (err) {
+								return next(err);
+							}
+
+							res.redirect(item.url);
+						});
+					}
+				}
+			);
+		}
+	},
+];
 
 exports.item_delete_get = (req, res) => {
 	res.send("NOT IMPLEMENTED: Item delete GET");
